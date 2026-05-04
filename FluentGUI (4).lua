0f60c8b02e57221a0feb661dda0d2c9cd6cc14d1
robdipekks-cell <robdipekks@gmail.com>
2026-05-04 14:44:56 +0800

    Update FluentGUI (4).lua

--- FluentGUI (4).lua
+++ FluentGUI (4).lua
@@ -1,6 +1,9 @@
 -- // Fluent GUI Script
 
-local Fluent            = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
 local Players           = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
@@ -676,6 +679,37 @@ for _, def in ipairs(FARM_DEFS) do
     end
 end
 
+Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+
 -- =============================================
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+ 
+-- Ignore keys that are used by ThemeManager.
+-- (we dont want configs to save themes, do we?)
+SaveManager:IgnoreThemeSettings()
+ 
+-- You can add indexes of elements the save manager should ignore
+SaveManager:SetIgnoreIndexes({})
+ 
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+ 
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+ 
+ 
 Window:SelectTab(1)
-Fluent:Notify({Title="Loaded", Content="Welcome "..LPN.."!", Duration=4})
+ 
+Fluent:Notify({
+    Title = "Fluent",
+    Content = "The script has been loaded.",
+    Duration = 8
+})
+ 
+-- You can use the SaveManager:LoadAutoloadConfig() to load a config
+-- which has been marked to be one that auto loads!
+SaveManager:LoadAutoloadConfig()
